9a300db235d58fcf1c96a9e922b83f2b9a5c3af5
jinxcapital <jinxcapital@protonmail.com>
2021-12-05 22:03:10 +0100

    :sparkles: Twitter utils

---
+++ src/utils/twitter.ts
@@ -0,0 +1,39 @@
+import Twitter from 'twitter';
+
+const twitter = new Twitter({
+  consumer_key: process.env.TWITTER_API_KEY as string,
+  consumer_secret: process.env.TWITTER_API_KEY_SECRET as string,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN as string,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET as string,
+});
+
+const uploadMedia = async (media: Buffer) => {
+  try {
+    const response = await twitter.post('/media/upload', {
+      media,
+    });
+
+    return response?.media_id_string || '';
+  } catch {
+    return '';
+  }
+};
+
+export const tweet = async (text: string, media?: Buffer) => {
+  const tweet = { status: text, media_ids: [] as string[] };
+  if (media) {
+    const mediaId = await uploadMedia(media);
+
+    if (mediaId) {
+      tweet.media_ids.push(mediaId);
+    }
+  }
+
+  try {
+    await twitter.post('statuses/update', tweet);
+  } catch (e) {
+    return false;
+  }
+
+  return true;
+};
