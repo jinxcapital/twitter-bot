273197bba3eecf85253b10839f8c92bca81f0f78
jinxcapital <jinxcapital@protonmail.com>
2021-12-05 22:14:49 +0100

    :wrench: ping only once an hour

--- src/crons.ts
+++ src/crons.ts
@@ -2,5 +2,5 @@ import cron from 'node-cron';
 
 import tasks from './tasks';
 
-cron.schedule('* * * * *', tasks.ping);
+cron.schedule('* 00 * * *', tasks.ping);
 cron.schedule('30 05 * * *', tasks.gm);
