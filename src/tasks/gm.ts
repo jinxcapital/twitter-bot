686480de25f3bd042cb59bcfc418c7b76b3a4050
jinxcapital <jinxcapital@protonmail.com>
2021-12-05 21:22:21 +0100

    :construction: gm every day at 05:30 UTC

--- src/tasks/gm.ts
+++ src/tasks/gm.ts
@@ -1,5 +1,16 @@
-// import TwitterAPI from 'services/twitter';
+import fetch from 'node-fetch';
 
 export default async () => {
-  // await TwitterAPI.tweet('hello world');
+  try {
+    const coin = await (
+      await fetch('https://api.jinx.capital/coins/btc')
+    ).json();
+    const chart = await (
+      await fetch('https://api.jinx.capital/chart/btc:usd.jpg')
+    ).buffer();
+
+    console.log({ coin, chart });
+  } catch (e) {
+    console.error(e);
+  }
 };
