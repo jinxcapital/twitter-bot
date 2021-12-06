c4c4b7a6650c5ec084531839c90f3d5c33f79ead
jinxcapital <jinxcapital@protonmail.com>
2021-12-06 21:23:15 +0100

    :sparkles: Add exchange netflow

--- src/tasks/gm.ts
+++ src/tasks/gm.ts
@@ -9,6 +9,9 @@ export default async () => {
     const chart = await (
       await fetch('https://api.jinx.capital/chart/btc:usd.jpg')
     ).buffer();
+    const { data: exchangeFlows } = await (
+      await fetch('https://api.jinx.capital/exchange-flows/bitcoin')
+    ).json();
 
     const percentageFormatter = new Intl.NumberFormat('en-US', {
       style: 'percent',
@@ -19,16 +22,25 @@ export default async () => {
       notation: 'compact',
       maximumFractionDigits: 1,
     });
+    const bigNumberFormatter = Intl.NumberFormat('en-US', {
+      notation: 'compact',
+      maximumFractionDigits: 1,
+      signDisplay: 'always',
+    });
 
-    const text = `GM! In the last 24 hours Bitcoin is ${
+    let text = `GM! In the last 24 hours Bitcoin is ${
       coin.percentageChange24h < 0 ? 'down' : 'up'
     } ${percentageFormatter.format(
       coin.percentageChange24h / 100,
-    )} and is currently sitting at ${dollarFormatter.format(
-      coin.price,
-    )}. How are we feeling about it today? #bitcoin $btc`;
+    )} and is currently sitting at ${dollarFormatter.format(coin.price)}.`;
 
-    console.log(text);
+    if (exchangeFlows.total.change.day) {
+      text += ` Exchange netflow: ${bigNumberFormatter.format(
+        exchangeFlows.total.change.day,
+      )} BTC.`;
+    }
+
+    text += ' How are we feeling about Bitcoin today? $bitcoin $btc';
 
     await tweet(text, chart);
   } catch (e) {
